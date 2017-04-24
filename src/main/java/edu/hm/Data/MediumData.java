142006ffcb4266a1f42fb5f876712ac58060d57b
Maximilian Schug <max.schug@googlemail.com>
2017-04-24 16:12:35 +0200

    filled Medium Data-Class

--- src/main/java/edu/hm/Data/MediumData.java
+++ src/main/java/edu/hm/Data/MediumData.java
@@ -1,9 +1,88 @@
 package edu.hm.Data;
 
 import edu.hm.Logic.MediumDataAccess;
+import edu.hm.model.Medium;
+import edu.hm.model.User;
+import org.w3c.dom.stylesheets.MediaList;
+
+import java.util.ArrayList;
 
 /**
  * Created by Maximilian on 21.04.2017.
  */
 public class MediumData implements MediumDataAccess {
+
+
+    private ArrayList<Medium> mediaList;
+
+
+    public void addMedium(User owner, String titel, String description, String Location) {
+        mediaList.add(new Medium(owner, titel, description, Location));
+    }
+
+    public ArrayList<Medium> findMediumByOwner(User owner){
+       ArrayList results = new ArrayList<Medium> ();
+        if(mediaList.size()>0){
+            for (Medium m: mediaList) {
+                if(m.getOwner()== owner){
+                    results.add(m);
+                }
+            }
+        }
+
+        return results;
+    }
+
+    public ArrayList<Medium> findMediumByTitel(String titel){
+        ArrayList results = new ArrayList<Medium> ();
+        if(mediaList.size()>0){
+            for (Medium m: mediaList) {
+                if(m.getTitel().contains(titel)){
+                    results.add(m);
+                }
+            }
+        }
+
+        return results;
+    }
+
+    public ArrayList<Medium> findMediumByDescribtion(String desc){
+        ArrayList results = new ArrayList<Medium> ();
+        if(mediaList.size()>0){
+            for (Medium m: mediaList) {
+                if(m.getDescription().contains(desc)){
+                    results.add(m);
+                }
+            }
+        }
+
+        return results;
+    }
+
+    public ArrayList<Medium> findMediumByLocation(String loc){
+        ArrayList results = new ArrayList<Medium> ();
+        if(mediaList.size()>0){
+            for (Medium m: mediaList) {
+                if(m.getLocation().contains(loc)){
+                    results.add(m);
+                }
+            }
+        }
+
+        return results;
+    }
+
+    public ArrayList<Medium> findMediumByBorrower(User borrower){
+        ArrayList results = new ArrayList<Medium> ();
+        if(mediaList.size()>0){
+            for (Medium m: mediaList) {
+                if(m.getBorrowedBy()==borrower){
+                    results.add(m);
+                }
+            }
+        }
+
+        return results;
+    }
+
 }
--- src/main/java/edu/hm/Data/UserData.java
+++ src/main/java/edu/hm/Data/UserData.java
@@ -1,9 +1,15 @@
 package edu.hm.Data;
 
 import edu.hm.Logic.UserDataAccess;
+import edu.hm.model.User;
+
+import java.util.ArrayList;
 
 /**
  * Created by Maximilian on 21.04.2017.
  */
 public class UserData implements UserDataAccess{
+
+
+    private ArrayList<User> userList;
 }
