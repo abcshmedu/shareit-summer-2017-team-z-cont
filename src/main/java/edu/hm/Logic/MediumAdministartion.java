c468956b22735db491e3deed88646d9d1fb9232c
Markus Kral <kral0@hm.edu>
2017-04-26 13:07:18 +0200

    did some stuff as discussed or not

--- src/main/java/edu/hm/Logic/MediumAdministartion.java
+++ src/main/java/edu/hm/Logic/MediumAdministartion.java
@@ -1,9 +1,12 @@
 package edu.hm.Logic;
 
 import edu.hm.model.Book;
+import edu.hm.model.Disc;
 import edu.hm.model.Medium;
 import edu.hm.Data.MediumData;
 import edu.hm.model.User;
+
+import java.util.ArrayList;
 
 /**
  * Created by Maximilian on 21.04.2017.
@@ -25,6 +28,122 @@ public class MediumAdministartion {
         return result;
     }
 
+
+    public ArrayList<Book> findMediumByISBN(String isbn){
+        ArrayList results = new ArrayList<Medium> ();
+        if(mdata.getMediaList().size() > 0){
+            for(Medium m: mdata.getMediaList()){
+                if (m instanceof Book){
+                    if (((Book) m).getISBN().equals(isbn))
+                        results.add(m);
+                }
+            }
+        }
+        return results;
+    }
+
+
+    public ArrayList<Disc> findMediumByBarcode(int barcode){
+        ArrayList results = new ArrayList<Disc> ();
+        if(mdata.getMediaList().size() > 0){
+            for(Medium m: mdata.getMediaList()){
+                if (m instanceof Disc){
+                    if (((Disc) m).getBARCODE() == barcode)
+                        results.add((Disc) m);
+                }
+            }
+        }
+        return results;
+    }
+
+    public ArrayList<Book> getAllBooks(){
+        ArrayList results = new ArrayList<Medium> ();
+        if(mdata.getMediaList().size() > 0){
+            for(Medium m: mdata.getMediaList()){
+                if (m instanceof Book)
+                        results.add((Book) m);
+                }
+            }
+        return results;
+    }
+
+    public ArrayList<Disc> getAllDiscs(){
+        ArrayList results = new ArrayList<Disc> ();
+        if(mdata.getMediaList().size() > 0){
+            for(Medium m: mdata.getMediaList()){
+                if (m instanceof Disc)
+                    results.add((Disc) m);
+            }
+        }
+        return results;
+    }
+
+
+
+    public ArrayList<Medium> findMediumByOwner(User owner){
+        ArrayList results = new ArrayList<Medium> ();
+        if(mdata.getMediaList().size()>0){
+            for (Medium m: mdata.getMediaList()) {
+                if(m.getOwner()!=null&&m.getOwner() == owner){
+                    results.add(m);
+                }
+            }
+        }
+
+        return results;
+    }
+
+    public ArrayList<Medium> findMediumByTitel(String titel){
+        ArrayList results = new ArrayList<Medium> ();
+        if(mdata.getMediaList().size()>0){
+            for (Medium m: mdata.getMediaList()) {
+                if(m.getTitel()!=null&&m.getTitel().contains(titel)){
+                    results.add(m);
+                }
+            }
+        }
+
+        return results;
+    }
+
+    public ArrayList<Medium> findMediumByDescribtion(String desc){
+        ArrayList results = new ArrayList<Medium> ();
+        if(mdata.getMediaList().size()>0){
+            for (Medium m: mdata.getMediaList()) {
+                if(m.getDescription()!=null&&m.getDescription().contains(desc)){
+                    results.add(m);
+                }
+            }
+        }
+
+        return results;
+    }
+
+    public ArrayList<Medium> findMediumByLocation(String loc){
+        ArrayList results = new ArrayList<Medium> ();
+        if(mdata.getMediaList().size()>0){
+            for (Medium m: mdata.getMediaList()) {
+                if(m.getLocation()!=null&&m.getLocation().contains(loc)){
+                    results.add(m);
+                }
+            }
+        }
+
+        return results;
+    }
+
+    public ArrayList<Medium> findMediumByBorrower(User borrower){
+        ArrayList results = new ArrayList<Medium> ();
+        if(mdata.getMediaList().size()>0){
+            for (Medium m: mdata.getMediaList()) {
+                if(m.getBorrowedBy()!=null&&m.getBorrowedBy()==borrower){
+                    results.add(m);
+                }
+            }
+        }
+
+        return results;
+    }
 
 
     public boolean checkValidISBN(String isbn){return true;}
--- src/main/java/edu/hm/REST/media_books.java
+++ src/main/java/edu/hm/REST/media_books.java
@@ -1,25 +1,55 @@
 package edu.hm.REST;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Produces;
-import javax.ws.rs.Path;
-import edu.hm.Data.MediumData;
-import edu.hm.Data.UserData;
-import edu.hm.model.User;
-import edu.hm.model.Medium;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import edu.hm.Logic.MediumAdministartion;
+import edu.hm.model.Book;
+
+
+import java.util.ArrayList;
 
 // The Java class will be hosted at the URI path "/helloworld"
 @Path("/media/books")
 public class media_books {
 
-    // List all available Books.
+    private MediumAdministartion mAdm = new MediumAdministartion();
+
+
+    /**
+     * list all books.
+     * @return List of all books.
+     */
     @GET
-    @Produces("text/plain")
-    public String getClichedMessage() {
-        MediumData mdata = new MediumData();
-        User user = new User("test", "test");
-        // add medium just for some output
-        mdata.addMedium(user, "bla", "test", "muc");
-        return mdata.toString();
+    @Produces("application/json")
+    public Response getAllBooks() {
+        ArrayList<Book> retlist = new ArrayList<>();
+        retlist = mAdm.getAllBooks();
+        return Response
+                .status(200)
+                .entity(retlist.toString())
+                .build();
+    }
+
+    /**
+     * list all books with isbn.
+     * @param isbn isbn
+     * @return list of books with isbn
+     */
+    @GET
+    @Path("{p}")
+    public Response getBookByISBN(@PathParam("p") String isbn){
+        ArrayList<Book> retlist = new ArrayList<>();
+        retlist = mAdm.findMediumByISBN(isbn);
+        return Response
+                    .status(200)
+                    .entity(retlist.toString())
+                    .build();
+
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createBook(){
+
     }
 }
