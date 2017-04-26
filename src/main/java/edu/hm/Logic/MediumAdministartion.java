c320388c659aac4487e96234b974f3528215e33e
Markus Kral <kral0@hm.edu>
2017-04-26 11:16:09 +0200

    We need a class for handling all Medium-operations.
    Should we also include the functions from MeidumData here?

--- src/main/java/edu/hm/Logic/MediumAdministartion.java
+++ src/main/java/edu/hm/Logic/MediumAdministartion.java
@@ -1,7 +1,40 @@
 package edu.hm.Logic;
 
+import edu.hm.model.Book;
+import edu.hm.model.Medium;
+import edu.hm.Data.MediumData;
+import edu.hm.model.User;
+
 /**
  * Created by Maximilian on 21.04.2017.
  */
 public class MediumAdministartion {
+
+    MediumData mdata = new MediumData();
+
+    /**
+     * create new book and test if all is correct
+     */
+    public String createBook(String isbn, User owner, String titel, String description, String location){
+        String result = "something went wrong";
+        if(checkValidISBN(isbn) && checkUserOK()){
+            mdata.addMedium(new Book(isbn, owner, titel, description, location));
+            result = "OK";
+        }
+
+        return result;
+    }
+
+
+
+    public boolean checkValidISBN(String isbn){return true;}
+    public boolean checkValidBarcode(){return true;}
+
+    /**
+     * check if the user is ok.
+     * @return
+     */
+    public boolean checkUserOK(){return true;}
+
+
 }
