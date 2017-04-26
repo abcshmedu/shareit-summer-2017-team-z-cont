985060b32d2e15c5021bb04e2faa1936ee655fdf
Markus Kral <kral0@hm.edu>
2017-04-26 16:47:22 +0200

    startup

--- src/main/java/edu/hm/JettyStarter.java
+++ src/main/java/edu/hm/JettyStarter.java
@@ -2,6 +2,9 @@ package edu.hm;
 
 import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.webapp.*;
+import edu.hm.Data.MediumData;
+import edu.hm.Logic.MediumAdministartion;
+import edu.hm.REST.RestInter;
 
 /**
  * Start the application without an AppServer like tomcat.
@@ -20,11 +23,19 @@ public class JettyStarter {
      * @throws Exception might throw for several reasons.
      */
     public static void main(String... args) throws Exception {
+
+        MediumData mdata = new MediumData();
+        MediumAdministartion mAdm = new MediumAdministartion(mdata);
+        RestInter rest = new RestInter(mAdm);
+
+
         Server jetty = new Server(PORT);
         jetty.setHandler(new WebAppContext(WEBAPP_DIR, APP_URL));
         jetty.start();
         System.out.println("Jetty listening on port " + PORT);
         jetty.join();
+
+
     }
 
 }
