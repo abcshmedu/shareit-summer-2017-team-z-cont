b58ef9b69894bbad651de806bf25861f87835726
Markus Kral <kral0@hm.edu>
2017-04-28 11:08:08 +0200

    rest-calls for books and discs

---
+++ src/main/java/edu/hm/REST/Media_disks.java
@@ -0,0 +1,144 @@
+package edu.hm.REST;
+
+import javax.validation.constraints.Null;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import edu.hm.Logic.MediumAdministartion;
+import edu.hm.model.Book;
+import edu.hm.model.Disc;
+import edu.hm.model.User;
+
+import javax.ws.rs.core.MediaType;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+
+import java.util.ArrayList;
+
+// The Java class will be hosted at the URI path "/helloworld"
+@Path("/media/books")
+public class Media_disks {
+
+
+    /**
+     * create ctor to get the logic
+     */
+    private static MediaAdminAccess mAdm;
+
+
+    /**
+     * list all books.
+     * @return List of all books.
+     */
+    @GET
+    @Produces("application/json")
+    public Response getAllDisvs() {
+        ArrayList<Disc> retlist = new ArrayList<>();
+        retlist = mAdm.getAllDiscs();
+        return Response
+                .status(200)
+                .entity(retlist.toString())
+                .build();
+    }
+
+    /**
+     * list all books with isbn.
+     * @param isbn isbn
+     * @return list of books with isbn
+     */
+    @GET
+    @Path("{p}")
+    public Response getDiscByBarcode(@PathParam("p") String barcode){
+        Disc result;
+        result = mAdm.findMediumByBarcode(barcode);
+        return Response
+                    .status(200)
+                    .entity(result.toString())
+                    .build();
+
+    }
+
+    @POST
+    @Consumes("application/json")
+    public Response createDisc(final String dataMsg){
+
+        JSONObject obj = new JSONObject(dataMsg);
+        String barcode;
+        String titel;
+        String description;
+        String user;
+        String password;
+        User user1 = null;
+
+        try {
+            barcode = obj.getString("barcode");
+            titel = obj.getString("titel");
+            description = obj.getString("description");
+            user = obj.getString("user");
+            password = obj.getString("password");
+            if(user != null && password != null){
+                user1 = new User(user, password);
+            } else {
+                user1 = new User("dummy", "dummy");
+            }        } catch (JSONException e) {
+            return Response.status(400)
+                    .entity("your json is invalid")
+                    .build();
+        }
+
+
+        String result = mAdm.createDisc(barcode, titel, description, user1);
+        return Response
+                .status(200)
+                .entity(result)
+                .build();
+    }
+
+    @PUT
+    @Consumes("application/json")
+    public Response updateDisc(final String dataMsg){
+        JSONObject obj = new JSONObject(dataMsg);
+
+        String barcode = null;
+        String titel = null;
+        String description = null;
+        String user = null;
+        String password = null;
+        User user1 = null;
+
+
+        try {
+            barcode = obj.getString("isbn");
+            titel = obj.getString("titel");
+            description = obj.getString("description");
+            user = obj.getString("user");
+            password = obj.getString("password");
+            if(user != null && password != null){
+                user1 = new User(user, password);
+            } else {
+                user1 = new User("dummy", "dummy");
+            }
+        } catch (JSONException e) {
+            return Response
+                    .status(401)
+                    .entity("your json is invalid")
+                    .build();
+        }
+
+
+        String result = mAdm.editDisc(barcode, title, description, user1);
+        return Response
+                .status(200)
+                .entity(result)
+                .build();
+
+    }
+
+
+
+        public static void setAccess(MediaAdminAccess mediaAccess) {
+        mAdm = mediaAccess;
+    }
+}
