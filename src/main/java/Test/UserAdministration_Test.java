8433f90c73c4d5b24933152bfbcb1b09214fb9c5
Maximilian Schug <max.schug@googlemail.com>
2017-05-28 14:42:02 +0200

    made test for User administration

--- src/main/java/Test/UserAdministration_Test.java
+++ src/main/java/Test/UserAdministration_Test.java
@@ -1,7 +1,77 @@
 package Test;
 
+import edu.hm.Data.UserData;
+import edu.hm.Logic.UserAdministartion;
+import edu.hm.model.User;
+
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertEquals;
+
 /**
  * Created by Maximilian on 26.04.2017.
  */
+@SuppressWarnings("CheckStyle")
 public class UserAdministration_Test {
+
+    public static void main(String... args){
+        UserAdministration_Test Tester = new UserAdministration_Test();
+        Tester.testFindUserCreateUser();
+        Tester.testLoginLogout();
+        Tester.testMakeAdminActivateUser();
+        Tester.testUserEditing();
+    }
+
+
+    public void testFindUserCreateUser(){
+        UserData userData = new UserData();
+        UserAdministartion userAdmin = new UserAdministartion(userData);
+        userAdmin.createUser("tester", "testPassword");
+        User testUser =  new User("tester", "testPassword");
+        assertEquals(userAdmin.findUser("tester"), testUser);
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(userData.findUserByUsername("AdminOne"));
+        userList.add(testUser);
+        assertEquals(userAdmin.getAllUsers(), userList);
+        userAdmin.createUser("second", "why", "Hans", "Lustig");
+        User second = new User("second", "why");
+        second.setForename("Hans");
+        second.setSurname("Lustig");
+        assertEquals(userAdmin.getAllUsers(), userList);
+    }
+
+    public void testLoginLogout(){
+        UserData userData = new UserData();
+        UserAdministartion userAdmin = new UserAdministartion(userData);
+        String token = userAdmin.logIn("AdminOne", "Admin");
+        assertEquals(true, userAdmin.checkToken(token));
+        userAdmin.logOut(token);
+        assertEquals(false, userAdmin.checkToken(token));
+    }
+
+    public void testMakeAdminActivateUser(){
+        UserData userData = new UserData();
+        UserAdministartion userAdmin = new UserAdministartion(userData);
+        userAdmin.createUser("tester", "testPassword");
+        String token = userAdmin.logIn("AdminOne", "Admin");
+        assertEquals(true, userAdmin.activateUser("tester", token));
+        assertEquals(true, userAdmin.makeAdmin("tester", token));
+        assertEquals(2, userAdmin.getAllAdmins(token).size());
+    }
+
+    public void testUserEditing(){
+        UserData userData = new UserData();
+        UserAdministartion userAdmin = new UserAdministartion(userData);
+        String token = userAdmin.logIn("AdminOne", "Admin");
+        userAdmin.changeForename("peter", token);
+        userAdmin.changeSurname("wurst", token);
+        User admin = new User("AdminOne", "Admin");
+        admin.setForename("peter");
+        admin.setSurname("wurst");
+        admin.setActivated(true);
+        admin.setAdmin(true);
+        assertEquals(admin, userAdmin.findUser("AdminOne"));
+    }
+
+
 }
